e1e52aabc6d189a46eef26a40ea715f6d8527087
dedalo6 <aperezdmor@educacion.navarra.es>
2024-01-09 19:31:18 +0100

    Inicio de proyecto

---
+++ VistasdeAritzPerezdeCiriza/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Vistas de Aritz Perez de Ciriza"
+include(":app")
+ 
