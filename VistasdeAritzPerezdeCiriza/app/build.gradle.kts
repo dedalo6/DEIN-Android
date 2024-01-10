c84a410da32b719c65f23804ae27128035788070
dedalo6 <aperezdmor@educacion.navarra.es>
2024-01-10 16:33:57 +0100

    Hecho

--- VistasdeAritzPerezdeCiriza/app/build.gradle.kts
+++ VistasdeAritzPerezdeCiriza/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.vistasdearitzperezdeciriza"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.vistasdearitzperezdeciriza"
